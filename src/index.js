18b88f3b384a14a96b2d67c6fd6cda0f9e577b13
Federico Bonomi <federico.bonomi89@gmail.com>
2017-01-20 17:07:10 +0100

    test redux

--- src/index.js
+++ src/index.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router'
+//import { Router, Route, hashHistory } from 'react-router'
+import { Provider } from 'react-redux'
 
-import Home from './pages/Home/Home';
-import About from './pages/About/About';
-import Info from './pages/Info/Info';
-import Users from './pages/Users/Users';
+//import About from './pages/About/About';
+//import Info from './pages/Info/Info';
+//import Users from './pages/Users/Users';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
+import { createStore } from 'redux'
+import todoApp from './reducers/index'
+import App from './pages/App/App';
+//import App from './components/App'
+
+let store = createStore(todoApp)
+
 ReactDOM.render(
-    <Router history={hashHistory}>
-        <Route path="/" component={Home}/>
-        <Route path="/about" component={About}/>
-        <Route path="/info" component={Info}/>
-        <Route path="/users" component={Users}/>
-    </Router>,
-  document.getElementById('root')
+    <Provider store={store}>
+    	<App />
+	</Provider>, 
+  	document.getElementById('root')
 );
